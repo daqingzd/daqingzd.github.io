1b54992f0e79effdd3ed9b131a078f5183080042
dqzhang <33880576@qq.com>
2023-10-20 21:44:33 +0800

    [Bug#35] Update pwaos shell/desktop/store

--- ubuntu/js/pwa.js
+++ ubuntu/js/pwa.js
@@ -1,13 +1,14 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('service-worker.js').then(function (registration) {
-        console.log('Registered!');
-        }, function (err) {
-            console.log('ServiceWorker registration failed: ', err);
-        }).catch(function (err) {
-            console.log(err);
-        });
-    });
-} else {
-    console.log('service worker is not supported');
+  // 由于 127.0.0.1:8000 是所有测试 Demo 的 host
+  // 为了防止作用域污染，将安装前注销所有已生效的 Service Worker
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    for (let reg of regs) {
+      reg.unregister()
+    }
+    navigator.serviceWorker.register('/ubuntu/sw.js', {scope: '/ubuntu/'}).then((registration) => {
+      console.log(`Service Worker registered! Scope: ${registration.scope}`);
+    },(error) => {
+       console.error(`Service Worker registration failed: ${error}`);
+    })
+  })
 }
